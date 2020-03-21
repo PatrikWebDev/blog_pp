2be3bbf0b685ad3900e44537b778932093329c62
P.Patrik <fake email>
2020-03-21 14:40:39 +0100

    date fixes

--- controllers/postControllers/new_post_controller.js
+++ controllers/postControllers/new_post_controller.js
@@ -10,7 +10,7 @@ function publishNewPost(req, res){
     }
 
     let currentdate = new Date(); 
-    let datetime = "Last Sync: " + currentdate.getDate() + "/"
+    let datetime = "Posted : " + currentdate.getDate() + "/"
                     + (currentdate.getMonth()+1)  + "/" 
                     + currentdate.getFullYear() + " @ "  
                     + currentdate.getHours() + ":"  
