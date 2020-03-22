ae7754e914b7e9e9ecbf1ae9ecbcc2e6a4594218
P.Patrik <fake email>
2020-03-22 13:43:45 +0100

    Deleted burned data

--- app.js
+++ app.js
@@ -23,26 +23,6 @@ app.use(express.urlencoded())
 let blogTitles = [
     "Elso blog oldalam"
 ]
-let blogPosts = [
-    {
-        author: "jani",
-        date: "2020.02.10",
-        title: "How to kung fu",
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ut orci sed justo iaculis gravida. Fusce congue sollicitudin leo et ullamcorper. Nunc finibus metus eu condimentum consequat. Sed ut nulla dolor. Vivamus blandit venenatis sollicitudin. Donec ac porta risus, id suscipit quam. In ut turpis ipsum."
-    }, 
-    {
-        author: "Tibi",
-        date: "2020.02.13",
-        title: "Mechanics",
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ut orci sed justo iaculis gravida. Fusce congue sollicitudin leo et ullamcorper. Nunc finibus metus eu condimentum consequat. Sed ut nulla dolor. Vivamus blandit venenatis sollicitudin. Donec ac porta risus, id suscipit quam. In ut turpis ipsum."
-    },
-    {
-        author: "krisz",
-        date: "2020.02.28",
-        title: "Main chemicals",
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ut orci sed justo iaculis gravida. Fusce congue sollicitudin leo et ullamcorper. Nunc finibus metus eu condimentum consequat. Sed ut nulla dolor. Vivamus blandit venenatis sollicitudin. Donec ac porta risus, id suscipit quam. In ut turpis ipsum."
-    }
-]
 
 
 
